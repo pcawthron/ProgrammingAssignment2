fd049ddfa5a961b50643d1dd0bfaa75f252640c3
Peter Cawthron <peter.cawthron@gmail.com>
2015-01-25 11:09:08 +0000

    Second Commit
    To see how github looks with two commits and how to identify the one you
    need.

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,14 @@
 ## R Programming (Coursera rprog-010)
 ## 25-Jan-2015
-##
+
+
 ## Creates a special "matrix" object that can cache its inverse
-
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
 }
